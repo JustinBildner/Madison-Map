42553e04e96ca23c75feebd04f067ebd12c56e9c
Nate Colburn <ncolburn2@wisc.edu>
2022-04-13 02:59:13 +0000

    adding interface

---
+++ IFrontendMadisonMap.java
@@ -0,0 +1,38 @@
+// --== CS400 Project Three File Header ==--
+// Name: Nathaniel Colburn
+// CSL Username: colburn
+// Email: ncolburn2@wisc.edu
+// Lecture #: 002 @1:00pm
+// Notes to Grader: <any optional extra notes to your grader>
+
+/**
+ * This interface provides the methods to make an interactive map of Madison to find the shortest
+ * path between two points
+ * 
+ * @author Nate Colburn
+ *
+ */
+public interface IFrontendMadisonMap {
+  /**
+   * Calls the back end shortestPath method and displays on the map the shortest path between two
+   * points
+   * 
+   * @param args An array of strings (points on the map) inputted from the webpage
+   */
+  void DiplayShortestPath(String[] args);
+
+  /**
+   * Calls the back end minimumTree method and displays the shortest path between multiple nodes
+   * 
+   * @param args An array of strings (points on the map) inputted from the webpage
+   */
+  void DisplayMinimumTree(String[] args);
+  
+  /**
+   * Main method that takes an array of strings from the website
+   * 
+   * @param args An array of strings (points on the map) inputted from the webpage
+   */
+  void main(String[] args);
+}
+
