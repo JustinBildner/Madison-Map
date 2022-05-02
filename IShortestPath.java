a10a4cd34474aa17364b134c6ea3d5619dc49e9d
Justin Bildner <justinbildner@Justins-MacBook-Pro.local>
2022-05-02 18:44:39 -0500

    IShortestPath.java

--- IShortestPath.java
+++ IShortestPath.java
@@ -1,10 +1,26 @@
+// --== CS400 Project Three File Header ==--
+// Name: Benjamin Miller
+// CSL Username: benm
+// Email: bhmiller3@wisc.edu
+// Lecture #: 002 @1:00pm
+// Notes to Grader: None
+
 import java.util.List;
 
+/**
+ * Interface for the ShortestPath object which provides the necessary methods to implement
+ * @author Benjamin Miller
+ *
+ */
 public interface IShortestPath {
-  /* FIELDS
+  /* POSSIBLE FIELDS
   public List<IVertex> vertices;
   public List<IEdge> edges;
   */
+  
+  // Accessor method to return the list of vertices included in the calculated shortest path
   public List<IVertex> getVertices();
+  
+  // Accessor method to return the list of edges included in the calculated shortest path
   public List<IEdge> getEdges();
 }
