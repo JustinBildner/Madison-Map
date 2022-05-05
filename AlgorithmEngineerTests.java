aaad9bd52ba0b03368e74134aefb0ba7de0bb5e4
Justin Bildner <justinbildner@Justins-MacBook-Pro.local>
2022-05-04 20:19:57 -0500

    adding Tests

--- AlgorithmEngineerTests.java
+++ AlgorithmEngineerTests.java
@@ -1,11 +1,13 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
+import java.io.FileNotFoundException;
 import java.util.Hashtable;
+import java.util.List;
 import java.util.NoSuchElementException;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
 
 /**
  * Tests the implementation of MadisonMap for
@@ -21,9 +23,15 @@ public class AlgorithmEngineerTests {
      */
     @BeforeEach
     public void createGraph() {
-        IMapLoader mapLoader = new IMapLoader();
-        Hashtable<String, IVertex> locations = new Hashtable<>();
-        map = new MadisonMap(mapLoader.loadMap());
+        try {
+            IMapLoader mapLoader = new MapLoader();
+            Hashtable<String, IVertex> locations = new Hashtable<>();
+            map = new MadisonMap(mapLoader.loadMap());
+        }
+        catch(FileNotFoundException e)
+        {
+            System.out.println("You are a dumbass");
+        }
 
 
 
@@ -71,6 +79,7 @@ public class AlgorithmEngineerTests {
         ));
     }
 
+
     /**
      * Checks the list of Edges in the ShortestPath object making sure they are correct for the
      * Minimum spanning tree computed using Primm's starting at Vertex A
@@ -115,6 +124,43 @@ public class AlgorithmEngineerTests {
         assertThrows(NoSuchElementException.class, () -> { graph.minTreeVert("C");});
     }
 
+    /**
+     * Checks the list of Vertices in the ShortestPath object making sure they are correct for the
+     * Minimum spanning tree computed using Primm's starting at Vertex A
+     */
+    @Test
+    public void testMadisonMinTreeBrats() {
+        System.out.println(map.minTreeVert("State Street Brats").size());
+        System.out.println(map.minTreeEdge("State Street Brats").size());
+//        assertTrue(graph.minTreeVert("A").toString().equals(
+//            "[A, F, E, D, C, B, G]"
+//        ));
+    }
+
+    /**
+     * Checks the list of Vertices in the ShortestPath object making sure they are correct for the
+     * Minimum spanning tree computed using Primm's starting at Vertex A
+     */
+    @Test
+    public void testMadisonMinTreeDanny() {
+
+        System.out.println(map.minTreeVert("Danny's Pub"));
+//        System.out.println(map.minTreeEdge("Danny's Pub"));
+                assertTrue(graph.minTreeVert("Danny's Pub").toString().equals(
+                    "[Danny's Pub, Le C's, Parthenon Gyros, Ian's Pizza, Madison Concourse Hotel, Wisconsin State Capitol, Basset Street Brunch, Basset Street Brunch, Tornado Stake House, RED, Washington & Broom, State Street Brats, University Book Store, Langdon & Lake, Lowell Center, Memorial Union, Bascom Hill, Chadbourne Hall, Washington & Basset, Washington & Basset, Nicholas Recreation Center, Witte Residence Hall]"));
+    }
+
+
+    @Test
+    public void testForAllVertices() {
+        MapLoader ml = new MapLoader();
+        try {
+            Hashtable<String, IVertex> vertices = ml.loadMap();
+            assertEquals(vertices.size(), 22, "Improper number of vertices loaded");
+        } catch (Exception e) {
+            fail("An Exception with the following message was thrown: " + e.getMessage());
+        }
+    }
 
 //    /**
 //     * Checks the distance/total weight cost from the vertex A to F.
--- Makefile
+++ Makefile
@@ -13,52 +13,40 @@ IMadisonMap.class: MadisonMap.java GraphADT.class
 	javac IMadisonMap.java
 GraphADT.class: GraphADT.java
 	javac GraphADT.java
-
 Edge.class: Edge.java IEdge.java
 	javac Edge.java
 	javac IEdge.java
-
 Vertex.class: Vertex.java IVertex.java
 	javac Vertex.java
 	javac IVertex.java
-
 Path.class: Path.java IShortestPath.java
 	javac Path.java
 	javac IShortestPath.java
-
 ShortestPath.class: ShortestPath.java IShortestPath.java
 	javac ShortestPath.java
 	javac IShortestPath.java
 
-run: FDBackendMadisonMap.class FrontendMadisonMap.class FDEdge.class FDIEdge.class FDIShortestPath.class FDIVertex.class FDShortestPath.class FDVertex.class
-	java FrontendMadisonMap
 
 runBackendDeveloperTests: BackendDeveloperTests.class
 	java -jar junit5.jar -cp . --scan-classpath -n BackendDeveloperTests
 
 BackendDeveloperTests.class: Edge.class IEdge.class IMadisonMap.class BMadisonMap.class IMapLoader.class BMapLoader.class IShortestPath.class ShortestPath.class GraphADT.class IMadisonMapBackend.class MadisonMapBackend.class BackendDeveloperTests.java
 	javac -cp .:junit5.jar BackendDeveloperTests.java
-
 IMadisonMap.class: IMadisonMap.java
 	javac IMadisonMap.java
-
 BMadisonMap.class: BMadisonMap.java
 	javac BMadisonMap.java
-
 IMapLoader.class: IMapLoader.java
 	javac IMapLoader.java
-
 BMapLoader.class: BMapLoader.java
 	javac BMapLoader.java
-
 GraphADT.class: GraphADT.java
 	javac GraphADT.java
-
 IMadisonMapBackend.class: IMadisonMapBackend.java
 	javac IMadisonMapBackend.java
-
 MadisonMapBackend.class: MadisonMapBackend.java
 	javac MadisonMapBackend.java
+
 
 clean:
 	@rm *.class
